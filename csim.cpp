b0fd609f66b1b131f30952b9db40ecc736be5e49
Nadia <nadiamomtaz05@gmail.com>
2025-10-22 02:01:35 -0400

    baseline memory hit

--- csim.cpp
+++ csim.cpp
@@ -4,3 +4,23 @@ int csim( int argc, char **argv ) {
 
   return 0;
 }
+typedef struct direct_map_element {
+    int tag;
+    bool valid;
+    int index;
+} direct_map_element;
+
+// determine if value is in cache
+bool memory_hit(direct_map_element *cache, direct_map_element e){
+  int idx = e.index;
+  direct_map_element cache_e = *(cache+ idx);
+  if (cache_e.valid == true && cache_e.tag == e.tag) {
+    return true;
+  }
+  return false;
+}
+
+// if value not in cache, go to memory to retrieve
+void load_from_memory() {
+
+}
